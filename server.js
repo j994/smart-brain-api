24afd7abed20d4b87485d23df335b0815ea8c772
Jeff Landers <jeff.landers12@gmail.com>
2020-01-15 20:11:46 +0100

    security fixes

--- server.js
+++ server.js
@@ -23,13 +23,17 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) =>{
+    const {email, password} = req.body;
+    if ( !email || !password){
+        return res.status(400).json('incorrect form submission')
+    }
     db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            const isValid = bcrypt.compareSync(password, data[0].hash)
             if (isValid) {
                 return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
+                    .where('email', '=', email)
                     .then(user => {
                         res.json(user[0])
                     })
@@ -43,6 +47,9 @@ app.post('/signin', (req, res) =>{
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    if (!email || !name || !password){
+        return res.status(400).json('incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password);
         db.transaction(trx => {
             trx.insert({
